eb8284ef8e2b8d0891aa55e7ba30ab2ebf25abaf
Alex Rodrigues <alex.rodrigues.developer@gmail.com>
2020-11-03 07:59:40 -0300

    voltando inkwell antigo

--- lib/widgets/product_item.dart
+++ lib/widgets/product_item.dart
@@ -21,35 +21,35 @@ class ProductItem extends StatelessWidget {
 
     return ClipRRect(
       borderRadius: BorderRadius.circular(10),
-      child: InkWell(
-        borderRadius: BorderRadius.circular(15),
-        splashColor: Colors.purple,
-        onTap: () => goToDetails(context, product),
-        child: GridTile(
+      child: GridTile(
+        child: InkWell(
+          borderRadius: BorderRadius.circular(15),
+          splashColor: Colors.purple,
+          onTap: () => goToDetails(context, product),
           child: Image.network(
             product.imageUrl,
             fit: BoxFit.cover,
           ),
-          footer: GridTileBar(
-            leading: IconButton(
-              icon: Icon(
-                product.isFavorite ? Icons.favorite : Icons.favorite_border,
-              ),
-              onPressed: () {
-                product.toggleFavorite();
-              },
-              color: Theme.of(context).accentColor,
+        ),
+        footer: GridTileBar(
+          leading: IconButton(
+            icon: Icon(
+              product.isFavorite ? Icons.favorite : Icons.favorite_border,
             ),
-            trailing: IconButton(
-              icon: Icon(Icons.shopping_cart),
-              onPressed: () {},
-              color: Theme.of(context).accentColor,
-            ),
-            backgroundColor: Colors.black87,
-            title: Text(
-              product.title,
-              textAlign: TextAlign.center,
-            ),
+            onPressed: () {
+              product.toggleFavorite();
+            },
+            color: Theme.of(context).accentColor,
+          ),
+          trailing: IconButton(
+            icon: Icon(Icons.shopping_cart),
+            onPressed: () {},
+            color: Theme.of(context).accentColor,
+          ),
+          backgroundColor: Colors.black87,
+          title: Text(
+            product.title,
+            textAlign: TextAlign.center,
           ),
         ),
       ),
