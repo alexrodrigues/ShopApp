702940d570176d9dcfbcc5ea08b000fe7416de29
Alex Rodrigues <alex.rodrigues.developer@gmail.com>
2020-11-04 07:34:34 -0300

    consumer

--- lib/widgets/product_item.dart
+++ lib/widgets/product_item.dart
@@ -17,52 +17,55 @@ class ProductItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final product = Provider.of<Product>(context);
-
-    return ClipRRect(
-      borderRadius: BorderRadius.circular(10),
-      child: GridTile(
-        child: Stack(
-          children: [
-            Positioned.fill(
-                child: Image.network(
-              product.imageUrl,
-              fit: BoxFit.cover,
-            )),
-            Positioned.fill(
-              child: Material(
-                color: Colors.transparent,
-                child: InkWell(
-                  borderRadius: BorderRadius.circular(15),
-                  splashColor: Theme.of(context).accentColor,
-                  onTap: () => goToDetails(context, product),
+    // nao precisa ser a raiz pode ser so o widget que voce quer ouvir
+    return Consumer<Product>(
+      builder: (ctx, product, child) {
+        return ClipRRect(
+          borderRadius: BorderRadius.circular(10),
+          child: GridTile(
+            child: Stack(
+              children: [
+                Positioned.fill(
+                    child: Image.network(
+                  product.imageUrl,
+                  fit: BoxFit.cover,
+                )),
+                Positioned.fill(
+                  child: Material(
+                    color: Colors.transparent,
+                    child: InkWell(
+                      borderRadius: BorderRadius.circular(15),
+                      splashColor: Theme.of(context).accentColor,
+                      onTap: () => goToDetails(context, product),
+                    ),
+                  ),
                 ),
+              ],
+            ),
+            footer: GridTileBar(
+              leading: IconButton(
+                icon: Icon(
+                  product.isFavorite ? Icons.favorite : Icons.favorite_border,
+                ),
+                onPressed: () {
+                  product.toggleFavorite();
+                },
+                color: Theme.of(context).accentColor,
+              ),
+              trailing: IconButton(
+                icon: Icon(Icons.shopping_cart),
+                onPressed: () {},
+                color: Theme.of(context).accentColor,
+              ),
+              backgroundColor: Colors.black87,
+              title: Text(
+                product.title,
+                textAlign: TextAlign.center,
               ),
             ),
-          ],
-        ),
-        footer: GridTileBar(
-          leading: IconButton(
-            icon: Icon(
-              product.isFavorite ? Icons.favorite : Icons.favorite_border,
-            ),
-            onPressed: () {
-              product.toggleFavorite();
-            },
-            color: Theme.of(context).accentColor,
           ),
-          trailing: IconButton(
-            icon: Icon(Icons.shopping_cart),
-            onPressed: () {},
-            color: Theme.of(context).accentColor,
-          ),
-          backgroundColor: Colors.black87,
-          title: Text(
-            product.title,
-            textAlign: TextAlign.center,
-          ),
-        ),
-      ),
+        );
+      },
     );
   }
 }
